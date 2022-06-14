b3e63387d05757ef98b2f222e5568f0cf927d1b2
pihda <62319714+pihdastudios@users.noreply.github.com>
2022-06-14 22:39:38 +0700

    Initial commit

---
+++ src/main/java/com/binar/teekmustbe/TeekMustBeApplication.java
@@ -0,0 +1,13 @@
+package com.binar.teekmustbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeekMustBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeekMustBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/binar/teekmustbe/TeekMustBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binar.teekmustbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeekMustBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
