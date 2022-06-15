fd4cf1aa36c1d53f0c39476b1e2db1a8491b30ee
Zhaldin <zhaldinsetiawan@gmai.com>
2022-06-15 21:53:35 +0700

    Menyiapkan kelas untuk Save User

---
+++ src/main/java/com/binar/teekmustbe/service/UserDetailsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.binar.teekmustbe.service;
+
+import com.binar.teekmustbe.dto.LoginDto;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements LoginService, UserDetailsService {
+
+
+    public void saveUser(LoginDto loginDto) {
+
+    }
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
