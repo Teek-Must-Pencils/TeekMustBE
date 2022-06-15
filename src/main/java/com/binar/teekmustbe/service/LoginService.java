0d480418c11a350aa9dae5194a9b997ce6989c32
Zhaldin <zhaldinsetiawan@gmai.com>
2022-06-15 21:50:27 +0700

    membuat kelas interface untuk kelas LoginService.java

--- src/main/java/com/binar/teekmustbe/service/LoginService.java
+++ src/main/java/com/binar/teekmustbe/service/LoginService.java
@@ -1,4 +1,12 @@
 package com.binar.teekmustbe.service;
 
+import com.binar.teekmustbe.dto.LoginDto;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 public interface LoginService {
+
+    void saveUser(LoginDto loginDto);
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+
 }
