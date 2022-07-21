eb735e5da00d75f99403c4483456fe9c8de68fc9
Zhaldin <zhaldinsetiawan@gmai.com>
2022-07-21 17:57:11 +0700

    Add endpoint get by id

--- src/main/java/com/binar/teekmustbe/controller/OfferController.java
+++ src/main/java/com/binar/teekmustbe/controller/OfferController.java
@@ -19,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.util.stream.Collectors;
 
 
@@ -56,5 +57,15 @@ public class OfferController {
             return new ResponseEntity<>(HttpStatus.OK);
         }
         return new ResponseEntity<>("Offer Not Found", HttpStatus.NOT_FOUND);
+    }
+
+    @Operation(summary = "Find Offer")
+    @GetMapping("Offer/{id}")
+    public ResponseEntity<?> findOfferById(@Valid @PathVariable("id") long id) {
+        var offer = offerService.findById(id);
+        if (offer.isEmpty()) {
+            return new ResponseEntity<>("Offer id" + id + " not found", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(new OfferDto(offer.get()), HttpStatus.OK);
     }
 }
