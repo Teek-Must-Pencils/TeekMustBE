40956c306cc27afd9bdb56575e2c3a555fcf3ee2
Adib <3923297-igdoma@users.noreply.gitlab.com>
2022-07-22 21:51:46 +0700

    remove unused dependency

--- src/main/java/com/binar/teekmustbe/controller/ProductController.java
+++ src/main/java/com/binar/teekmustbe/controller/ProductController.java
@@ -2,20 +2,15 @@ package com.binar.teekmustbe.controller;
 
 
 import com.binar.teekmustbe.dto.ProductDto;
-
-
 import com.binar.teekmustbe.service.product.ProductService;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.stream.Collectors;
